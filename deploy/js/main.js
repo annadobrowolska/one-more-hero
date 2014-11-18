532e75afab373eec056b7a7cd2d8a9186ebd91b6
Anna Dobrowolska <anna.dobrowolska@poczta.pl>
2014-11-18 12:34:56 +0100

    Dołączenie pełnego pliku Phasera (zamiast wersji okrojonej)

--- deploy/js/main.js
+++ deploy/js/main.js
@@ -1,11 +1,3 @@
-/**
- *
- * This is a simple state template to use for getting a Phaser game up
- * and running quickly. Simply add your own game logic to the default
- * state object or delete it and make your own.
- *
- */
-
 var state = {
     init: function () {
 
