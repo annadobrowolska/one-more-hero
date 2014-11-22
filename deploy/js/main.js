6113d9e27c0c4a40875a802cc079d3420337881c
Anna Dobrowolska <anna.dobrowolska@poczta.pl>
2014-11-22 15:53:04 +0100

    Reorganizacja funkcji

--- deploy/js/main.js
+++ deploy/js/main.js
@@ -1,27 +1,16 @@
-var state = {
-    init: function () {
+var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 
-        var text = "Phaser Version " + Phaser.DEV_VERSION + " works!";
-        var style = { font: "24px Arial", fill: "#fff", align: "center" };
-        var t = game.add.text(this.world.centerX, this.world.centerY, text, style);
-        t.anchor.setTo(0.5, 0.5);
+var level = null;
+var text = "Phaser Version " + Phaser.DEV_VERSION + " works!";
+var style = { font: "24px Arial", fill: "#fff", align: "center" };
+var t = game.add.text(this.world.centerX, this.world.centerY, text, style);
+t.anchor.setTo(0.5, 0.5);
 
-    },
-    preload: function () {
-        // STate preload logic goes here
-    },
-    create: function () {
-        // State create logic goes here
-    },
-    update: function () {
-        // State Update Logic goes here.
-    }
-};
+function preload() {
+}
+function create() {
+}
+function update() {
+}
 
-var game = new Phaser.Game(
-    800,
-    480,
-    Phaser.AUTO,
-    'game',
-    state
-);
+
