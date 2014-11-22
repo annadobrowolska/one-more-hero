0ba3b0e28787c1e6bc4ec79477666268dab2c134
Anna Dobrowolska <anna.dobrowolska@poczta.pl>
2014-11-22 17:53:41 +0100

    Dodanie sterowania postacią

--- deploy/js/Player.js
+++ deploy/js/Player.js
@@ -1,7 +1,33 @@
+this.game.physics.arcade.gravity.y = 1869;
+this.game.physics.arcade.enableBody(this.player);
+
+this.cursors = this.game.input.keyboard.createCursorKeys();
+this.player.body.drag.setTo(600, 0); // zwalnianie postaci jak nie są naciskane klawisze (x,y)
+
+this.addSensitivityToKeys();
+},
+
+/**
+ * Supports player control. Function can be used only in Player class, don't use externally!
+ */
+addSensitivityToKeys: function () {
+    if (this.cursors.left.isDown) {
+        this.player.body.velocity.x = -this.SPEED;
+    }
+    else if (this.cursors.right.isDown) {
+        this.player.body.velocity.x = this.SPEED;
+    }
+    else if (this.cursors.up.isDown && this.player.body.touching.down) {
+        this.player.body.velocity.y = -700;
+    }
+}
+
 Player = function (game) {
 
     this.game = game;
     this.player = null;
+    this.cursors = null;
+    this.SPEED = 200;
 };
 
 Player.prototype = {
