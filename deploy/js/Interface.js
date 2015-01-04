0ce697bdc459c5a25f7623a3528ee6dd5b20b6fb
Anna Dobrowolska <anna.dobrowolska@poczta.pl>
2015-01-04 16:26:45 +0100

    Zaliczanie tylko minut i sekund, optymalizacja timera

--- deploy/js/Interface.js
+++ deploy/js/Interface.js
@@ -3,42 +3,33 @@ Interface = function (game) {
     this.score = 0;
     this.scoreText = null;
     this.timerText = null;
-    this.miliseconds = 0;
+    this.sec = 0;
 };
 
 Interface.prototype = {
     create: function () {
-        this.timerText = this.game.add.text(16, 16, "time: " + "  00:00:000", { fontSize: '32px', fill: '#ffffff' });
+        this.timerText = this.game.add.text(16, 16, "time: " + "  00:00", { fontSize: '32px', fill: '#ffffff' });
         this.timerText.fixedToCamera = true;
         this.scoreText = this.game.add.text(16, 40, 'score: 0', { fontSize: '32px', fill: '#ffffff' });
         this.scoreText.fixedToCamera = true;
+        this.game.time.events.loop(Phaser.Timer.SECOND, this.updateTime, this);
     },
 
     update: function (alive) {
-        if (alive == true) {
-            this.updateElapsedTime();
-        }
-        else {
+        if (alive == false) {
             this.game.time.removeAll();
         }
     },
 
-    updateElapsedTime: function () {
-        this.game.time.events.loop(Phaser.Timer.SECOND, this.updateTime, this);
-    },
-
     updateTime: function () {
-        this.miliseconds += 1;
+        this.sec += 1;
         this.timerText.setText("time: " + this.formattedTime())
     },
 
     formattedTime: function () {
-        var ms = this.miliseconds;
-        var sec = ms / 1000;
-        var min = sec / 60;
-        sec %= 60;
-        ms %= 1000;
-        return "  " + this.pad(min.toFixed(0), 2) + ":" + this.pad(sec.toFixed(0), 2) + ":" + this.pad(ms.toFixed(0), 3);
+        var min = this.sec / 60;
+        var formattedSec = this.sec % 60;
+        return "  " + this.pad(min.toFixed(0), 2) + ":" + this.pad(formattedSec.toFixed(0), 2);
     },
 
     pad: function (num, size) {
