46b61f388efe0cac0751621d74e4677847ba5846
Anna Dobrowolska <anna.dobrowolska@poczta.pl>
2015-01-31 16:32:08 +0100

    Dodanie ekranu głównego

--- deploy/js/main.js
+++ deploy/js/main.js
@@ -1,9 +1,24 @@
 var FIRST_LEVEL_STATE = "firstLevel";
+var MAIN_MENU = "mainMenu"
 
 var level = null;
 var player = null;
 var gameInterface = null;
 var TILE_SIZE = 64;
+
+var mainMenuState = {
+    preload: function () {
+        this.game.load.spritesheet('menu', 'assets/menu.png', 1024, 640);
+    },
+
+    create: function () {
+        this.add.sprite(0, 0, 'menu');
+    },
+
+    update: function () {
+
+    }
+};
 
 var firstLevelState = {
     preload: function () {
@@ -28,5 +43,6 @@ var firstLevelState = {
 };
 
 var game = new Phaser.Game(1024, 640, Phaser.AUTO, 'game');
+game.state.add(MAIN_MENU, mainMenuState);
 game.state.add(FIRST_LEVEL_STATE, firstLevelState);
-game.state.start(FIRST_LEVEL_STATE);
+game.state.start(MAIN_MENU);
